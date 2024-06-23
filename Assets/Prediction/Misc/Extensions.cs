3b4eadbe4c0b40d6bf1693c001180b48c33bbe40
Jazz23 <noreply>
2024-06-23 10:19:12 -0700

    Extensions.cs

--- Assets/Prediction/Misc/Extensions.cs
+++ Assets/Prediction/Misc/Extensions.cs
@@ -9,6 +9,13 @@ namespace Prediction
             x = vector.x;
             y = vector.y;
         }
+        
+        public static void Deconstruct(this Vector3 vector, out float x, out float y, out float z)
+        {
+            x = vector.x;
+            y = vector.y;
+            z = vector.z;
+        }
 
         public static Vector3 WithY(this Vector3 vector, float y)
         {
