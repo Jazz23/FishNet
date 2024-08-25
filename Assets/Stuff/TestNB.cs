b55db8044f11e5b8bc159633d362ec5932aa9b67
Jazz23 <noreply>
2024-08-25 08:26:32 -0700

    tick stuff

--- Assets/Stuff/TestNB.cs
+++ Assets/Stuff/TestNB.cs
@@ -1,4 +1,7 @@
-﻿using FishNet.Object;
+﻿using FishNet.Connection;
+using FishNet.Managing.Server;
+using FishNet.Managing.Timing;
+using FishNet.Object;
 using UnityEngine;
 
 namespace Stuff
@@ -7,7 +10,33 @@ namespace Stuff
     {
         public override void OnStartClient()
         {
-            Debug.Log("Started");
+        }
+
+        public void SendTestRpc()
+        {
+            TestRpc(TimeManager.GetPreciseTick(TickType.LastPacketTick));
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void TestRpc(PreciseTick tick, NetworkConnection conn = null)
+        {
+            if (tick.Tick > conn!.LocalTick.LocalTick)
+            {
+                conn.Kick(KickReason.MalformedData, log: "Out of order tick.");
+                return;
+            }
+            
+            Debug.Log("Received RPC");
+            var idfk = conn.PacketTick;
+            var timeDelta = TimeManager.TimePassed(tick);
+            var timeDelta2 = TimeManager.TimePassed(idfk.LastRemoteTick);
+            var timeDelta4 = TimeManager.TimePassed(conn.LocalTick.LocalTick);
+
+
+            var serverReceivedTick = TimeManager.GetPreciseTick(TickType.Tick);
+            var serverReceivedTick2 = TimeManager.GetPreciseTick(TickType.LastPacketTick);
+            var ticksMissed = serverReceivedTick.Tick - tick.Tick;
+            var timeDelta3 = ticksMissed * TimeManager.TickDelta;
         }
     }
 }
